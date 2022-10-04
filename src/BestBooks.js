bce3e52776c35cfb1123b4dc2c275c27a1c39f3f
Michael Kancov <michael.kancov00@gmail.com>
2022-10-03 22:02:58 -0700

    basically done with front-end

--- src/BestBooks.js
+++ src/BestBooks.js
@@ -1,4 +1,7 @@
 import React from 'react';
+import axios from 'axios';
+import Carousel from 'react-bootstrap/Carousel';
+//import Book from './Book.js';
 
 class BestBooks extends React.Component {
   constructor(props) {
@@ -10,18 +13,84 @@ class BestBooks extends React.Component {
 
   /* TODO: Make a GET request to your API to fetch all the books from the database  */
 
+  getBooks = async () => {
+    try {
+      let url = `${process.env.REACT_APP_SERVER}/books`;
+      let bookResponse = await axios.get(url);
+      let books = bookResponse.data;
+      console.log(books[0]);
+     this.setState({books:books}); 
+    } catch (error) {
+      console.log('error', error.response);
+    }
+  }
+
+  componentDidMount() {
+    this.getBooks();
+  }
+
   render() {
 
-    /* TODO: render all the books in a Carousel */
+    
 
     return (
+
       <>
+      {this.state.books.length ?( 
+      <Carousel>
+      <Carousel.Item>
+        <img
+          className="d-block w-100"
+          src="https://via.placeholder.com/800x400"
+          alt="First slide"
+        />
+        <Carousel.Caption>
+          <h3>{this.state.books[0].title}</h3>
+          <p>{this.state.books[0].description}</p>
+        </Carousel.Caption>
+      </Carousel.Item>
+      <Carousel.Item>
+        <img
+          className="d-block w-100"
+          src="https://via.placeholder.com/800x400"
+          alt="Second slide"
+        />
+
+        <Carousel.Caption>
+          <h3>{this.state.books[1].title}</h3>
+          <p>{this.state.books[1].description}</p>
+        </Carousel.Caption>
+      </Carousel.Item>
+      <Carousel.Item>
+        <img
+          className="d-block w-100"
+          src="https://via.placeholder.com/800x400"
+          alt="Third slide"
+        />
+
+        <Carousel.Caption>
+          <h3>{this.state.books[2].title}</h3>
+          <p>
+            {this.state.books[2].description}
+          </p>
+        </Carousel.Caption>
+      </Carousel.Item>
+    </Carousel>
+      ) : <p>No books to display</p> }
         <h2>My Essential Lifelong Learning &amp; Formation Shelf</h2>
 
         {this.state.books.length ? (
-          <p>Book Carousel coming soon</p>
+          <container>
+            {this.state.books.map((book, idx) => (
+              <div key={idx}>
+                <h3>{book.title}</h3>
+                <p>{book.description}</p>
+                <p>{book.status}</p>
+              </div>
+            ))}
+          </container>
         ) : (
-          <h3>No Books Found :(</h3>
+          <p>No books in the database.</p>
         )}
       </>
     )
